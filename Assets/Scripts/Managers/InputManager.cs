528952f3972b73b1570094925550c93559cbd69e
Kasper Andkjær <sswkasper@hotmail.com>
2014-03-22 19:32:39 +0100

    Setup Made

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && 
+			forEach(){
+				
+			}
+		  Input.GetTouch(0).phase == TouchPhase.Moved) {
+					
+			// Move object across XY plane
+		}
+	}
+}
